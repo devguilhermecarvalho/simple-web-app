953616f3aafb251d679b25480a2b44b8c4c424cb
Guilherme Rosa de Carvalho <79029084+devguilhermecarvalho@users.noreply.github.com>
2023-04-21 16:38:43 -0300

    Add files via upload

---
+++ Pages/Categories.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace MyRazorApp.Pages;
+
+public class Categories : PageModel
+{
+    public List<Category> CategoryList { get; set; } = new();
+   
+    public void OnGet(int skip = 0,
+                      int take = 75)
+    {
+        var temp = new List<Category>();
+        for(var i = 0; i <= 1787; i++)
+        {
+            temp.Add(
+                new Category(i,
+                    $"Categoria {i}",
+                    i * 18.98M));
+        }
+
+        CategoryList = temp
+            .Skip(skip)
+            .Take(take)
+            .ToList();
+    }
+}
+public record Category( //representa uma clase
+    int Id,
+    string Title,
+    decimal Price
+);
---
+++ Pages/index.cshtml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace MyRazorApp.Pages;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
